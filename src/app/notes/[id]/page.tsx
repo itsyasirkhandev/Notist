b6bded537654a0222d67fee91e4fecad348b6989
Yasir Khan <yasirwebio@gmail.com>
2025-10-02 07:55:25 +0000

    fix this issue " Server   Error: Route "/notes/[id]" used `params.id`. `

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
-export default function EditNotePage({ params }: { params: { id: string } }) {
+export default async function EditNotePage({ params }: { params: { id: string } }) {
+    const { id } = await params;
     return (
         <div className="flex flex-col items-center justify-start min-h-screen bg-background font-body selection:bg-primary/20">
             <div className='w-full bg-primary text-primary-foreground border-b-4 border-border'>
@@ -22,7 +23,7 @@ export default function EditNotePage({ params }: { params: { id: string } }) {
             </div>
             <div className="w-full p-4 md:pt-8">
                 <main>
-                    <NoteForm noteId={params.id} />
+                    <NoteForm noteId={id} />
                 </main>
             </div>
         </div>
