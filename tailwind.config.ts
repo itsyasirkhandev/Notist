c1e7a10fa3e4b2274e498956dc02d4914730f8e8
Yasir Khan <yasirwebio@gmail.com>
2025-10-05 05:17:43 +0000

    Integrate a TweakCN custom color theme into a Tailwind CSS v3 project. P

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,16 @@ export default {
           '4': 'hsl(var(--chart-4))',
           '5': 'hsl(var(--chart-5))',
         },
+         sidebar: {
+          DEFAULT: "hsl(var(--sidebar))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -64,7 +74,9 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        sans: ["var(--font-sans)", "Architects Daughter", "sans-serif"],
+        serif: ["var(--font-serif)", "Times New Roman", "serif"],
+        mono: ["var(--font-mono)", "Courier New", "monospace"],
       },
       boxShadow: {
         '2xs': 'var(--shadow-2xs)',
